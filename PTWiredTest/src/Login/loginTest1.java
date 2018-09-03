ed92c33771f83d0b0d17f21025055aad93a8f588
VeenaAdithya <40333205+VeenaAdithya@users.noreply.github.com>
2018-09-03 19:28:59 +0530

    3 Sept

--- PTWiredTest/src/Login/loginTest1.java
+++ PTWiredTest/src/Login/loginTest1.java
@@ -27,7 +27,7 @@ public class loginTest1 {
 	
 	@Test(priority=1)
 	
-	public void StartBrowser()
+	public void aStartBrowser()
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\veenaramakrishnan\\chromedriver.exe");
 		
@@ -39,9 +39,9 @@ public class loginTest1 {
 	
 	
 	
-	 @Test(dependsOnMethods="StartBrowser",description="This test is to verify if Login page accepts username")
+	 @Test(priority=2,dependsOnMethods="aStartBrowser",description="This test is to verify if Login page accepts username")
 	 
-	 public void EmailInputField()
+	 public void bEmailInputField()
 	 {
 		 
 		 //Open url
@@ -71,16 +71,16 @@ public class loginTest1 {
 	 
 	
 	
-	 @Test(dependsOnMethods="EmailInputField",description="This test is to verify if Login page accepts password")
+	 @Test(priority=3,dependsOnMethods="bEmailInputField",description="This test is to verify if Login page accepts password")
 	 
-	 public void PasswordInputField() throws IOException
+	 public void cPasswordInputField() throws IOException
 	 
 	 {
-		 //Assert pasword field in Login page is enabled
+		 //Assert password field in Login page is enabled
 		 
 		 Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div/div/div/div/div[2]/div/div[2]/form/div[2]/input")).isEnabled());
 		 
-		 Reporter.log("----------------Asserted--Password field in login page is enabled");
+		 Reporter.log("----------------Asserted--Password field in login page is enabled-----------------------");
 		 
 		 //Send values to password field
 		 
@@ -90,7 +90,7 @@ public class loginTest1 {
 		 //Take snapshot
 		 File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 
-		 File screenshotName = new File ("C:\\Users\\veenaramakrishnan\\git\\Coaching-Nexus\\CNX_TestSuites-QA\\Screenshots\\Suite02a\\01"+driver.getTitle()+".png");
+		 File screenshotName = new File ("C:\\Users\\veenaramakrishnan\\git\\PTWired\\PTWiredTest\\Screenshots\\loginTest1\\01"+driver.getTitle()+".png");
 
 		 FileUtils.copyFile(source, screenshotName);
 
@@ -100,8 +100,36 @@ public class loginTest1 {
 		 
 	 }
 	
+	@Test(priority=4,dependsOnMethods="cPasswordInputField",description="This test is to verify if Login button in Login page is enabled")
 	
+	public void dLoginButton() throws IOException
 	
+	{
+		Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div/div/div/div/div[2]/div/div[2]/form/div[3]/button")).isEnabled());
+		
+		Reporter.log("----------------Asserted--Login button is enabled------------------");
+		
+		//Click on Login
+		
+		driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div/div/div/div/div[2]/div/div[2]/form/div[3]/button")).click();
+		
+		 WebDriverWait wait = new WebDriverWait(driver,40);
+		 
+		 WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"root\"]/div/div[2]/div/div/div[2]/div/div/div/div/div/div/div/div[1]/div[2]/div/a/button")));
+		 
+		
+		 //Take snapshot
+		 File source1 = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
+		 File screenshotName1 = new File ("C:\\Users\\veenaramakrishnan\\git\\PTWired\\PTWiredTest\\Screenshots\\loginTest1\\02"+driver.getTitle()+".png");
+
+		 FileUtils.copyFile(source1, screenshotName1);
+
+
+		 Reporter.log("<br><img src='"+screenshotName1+"' height='400' width='850'/><br>");
+
+		
+	}
 	
 	
 	
